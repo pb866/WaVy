55ec7fe73f39fd819ba6612565dae2bc14a43d8f
Peter Bräuer <pb866.git@gmail.com>
2019-06-03 10:56:57 +0200

    Loop over exception until file opening successful

--- WaVyLib/filesOpen.py
+++ WaVyLib/filesOpen.py
@@ -11,9 +11,17 @@ class NewFilePath:
         return gdal.Open(askopenfilename())
 
     def openFile(self):
-         self.ds = self.getFilePath()
-         self.fname = os.path.basename(self.ds.GetDescription())
-         print('The image {} was loaded successfully'.format(self.fname))
+        success = False
+        while not success:
+            try:
+                self.ds = self.getFilePath()
+                self.fname = os.path.basename(self.ds.GetDescription())
+                print('The image {} was loaded successfully'.format(self.fname))
+                success = True
+            except:
+                # again=input("Image doesn't exist or is corrupted. Try again (y)?")
+                # if again.lower()[0] != 'y': success = True
+                print("Image doesn't exist or is corrupted. Try again!")
 
     def getDs(self):
         return self.ds
